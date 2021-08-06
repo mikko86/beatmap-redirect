fa8d7369b30c380cb686439ae2e01c2bd98fe949
Isak Løvås <76748478+isaklovas@users.noreply.github.com>
2021-08-06 23:32:15 +0200

    fixed redirecting for alternative beatmap link

--- userscript.user.js
+++ userscript.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         akatsuki redirect
 // @namespace    isak
-// @version      1.4
+// @version      1.5
 // @description  redirects you to the osu.ppy.sh beatmap page
 // @author       isak løvås
 // @match        http://akatsuki.pw/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @noframes
 // @include      http://osu.ppy.sh*
 // @include      https://old.ppy.sh*
 // @include      https://akatsuki.pw*
@@ -19,14 +18,19 @@
 
     var url = window.location.hostname;
     var urlPathname = window.location.pathname;
-    
+
     if (url == "akatsuki.pw" && urlPathname[8] == "s" && urlPathname[9] == "/") {
-        let a = confirm("Do you want to redirect to osu.ppy.sh beatmap page?")
+        let a = confirm("Do you want to redirect to osu.ppy.sh beatmap page?");
         if (a) {
-            window.open("https://osu.ppy.sh" + urlPathname)
+            window.open("https://osu.ppy.sh" + urlPathname);
         }
-    } else {
-        console.log("false")
+    } else if (url == "akatsuki.pw" && urlPathname[1] == "b" && urlPathname[2] == "/") {
+        let a = confirm("Do you want to redirect to osu.ppy.sh beatmap page?");
+        if (a) {
+            window.open("https://osu.ppy.sh" + urlPathname);
+    }else {
+        console.log("false");
     }
+}
 
 })();
