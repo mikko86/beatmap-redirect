36d6d183e0d3d9312221d72f40ec33b75fa39384
Isak Løvås <76748478+isaklovas@users.noreply.github.com>
2021-08-06 12:26:33 +0200

    fixed unknown issue, somehow

--- userscript.user.js
+++ userscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         akatsuki redirect
 // @namespace    isak
-// @version      1.2
+// @version      1.3
 // @description  redirects you to the osu.ppy.sh beatmap page
 // @author       isak løvås
 // @match        http://akatsuki.pw/*
@@ -10,10 +10,8 @@
 // @include      http://osu.ppy.sh*
 // @include      https://old.ppy.sh*
 // @include      https://akatsuki.pw*
-// @grant        GM.xmlHttpRequest
-// @grant        GM_xmlhttpRequest
-// @updateURL    https://raw.githubusercontent.com/isaklovas/beatmap-redirect/master/userscript.user.js
-// @downloadURL  https://raw.githubusercontent.com/isaklovas/beatmap-redirect/master/userscript.user.js
+// @updateURL    https://raw.githubusercontent.com/isaklovas/beatmap-redirect/master/userscript.js
+// @downloadURL  https://raw.githubusercontent.com/isaklovas/beatmap-redirect/master/userscript.js
 // ==/UserScript==
 
 (() => {
@@ -21,14 +19,14 @@
 
     var url = window.location.hostname;
     var urlPathname = window.location.pathname;
-
-    if (url === "akatsuki.pw" && urlPathname[1] === "b" && urlPathname[2] == "/" && urlPathname[10] == "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9") {
+    
+    if (url == "akatsuki.pw" && urlPathname[1] == "b" && urlPathname[2] == "/" || urlPathname[2] == "e") {
         let a = confirm("Do you want to redirect to osu.ppy.sh beatmap page?")
-        if (a === true && url === "akatsuki.pw" && urlPathname[1] === "b") {
-            window.open("https://osu.ppy.sh" + urlPathname);
-    } else {
-        return false;
-    }
+        if (a) {
+            window.open("https://osu.ppy.sh" + urlPathname)
         }
+    } else {
+        console.log("false")
+    }
 
 })();
